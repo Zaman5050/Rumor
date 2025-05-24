5151b3a31029b9ad067779510a92eb0baed61365
Zaman5050 <zamanarshad5050@gmail.com>
2025-05-24 15:32:34 +0500

    Rumor web-application

---
+++ Rumor Dashboard/assets/js/scripts.js
@@ -0,0 +1,43 @@
+
+(function($) {
+    "use strict";
+
+    /*================================
+    Preloader
+    ==================================*/
+
+    var preloader = $('#preloader');
+    $(window).on('load', function() {
+        setTimeout(function() {
+            preloader.fadeOut('slow', function() { $(this).remove(); });
+        }, 300)
+    });
+
+    /*================================
+    sidebar collapsing
+    ==================================*/
+    if (window.innerWidth <= 1364) {
+        $('.page-container').addClass('sbar_collapsed');
+    }
+    $('.nav-btn').on('click', function() {
+        $('.page-container').toggleClass('sbar_collapsed');
+    });
+
+   
+
+    
+
+   
+
+   
+
+    
+
+
+    
+
+   
+
+})(jQuery);
+
+
